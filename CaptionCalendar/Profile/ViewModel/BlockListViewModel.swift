1f8bb49c05a991eb28fb04838da8ea0dcb508f48
TAKAMA <caption.service@outlook.jp>
2021-11-01 18:37:00 +0900

    11/1

---
+++ CaptionCalendar/Profile/ViewModel/BlockListViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  BlockListViewModel.swift
+//  CaptionCalendar
+//
+//  Created by 髙間洋平 on 2021/10/21.
+//
+
+import SwiftUI
+
+struct BlockListViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BlockListViewModel_Previews: PreviewProvider {
+    static var previews: some View {
+        BlockListViewModel()
+    }
+}
